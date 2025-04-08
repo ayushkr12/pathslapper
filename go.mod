132a642d81d860bb1e79058f11ce4c63446aa1d7
ayushkr12 <ayushkr91997@gmail.com>
2025-04-08 13:04:09 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ayushkr12/pathslapper
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	urlparser "net/url"
+	"os"
+	"strings"
+)
+
+func showHelp() {
+	fmt.Println("\nUsage: cat <urls_filePath> | pathslapper <path-to-append>")
+	fmt.Println("Example: ")
+	fmt.Println("      cat urls.txt | pathslapper /api/v1/admin\n")
+}
+
+func removeURLQueryParams(url string) string {
+	parsedURL, _ := urlparser.Parse(url) // ignore error
+	parsedURL.RawQuery = ""
+	return parsedURL.String()
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		showHelp()
+		return
+	}
+	path := os.Args[1]
+
+	if !strings.HasPrefix(path, "/") {
+		path = "/" + path
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		parsedURL := removeURLQueryParams(scanner.Text())
+		newURL := strings.TrimRight(parsedURL, "/") + path
+		fmt.Println(newURL)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+	}
+}
